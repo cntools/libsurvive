c18735f2c8490f2d0040f11467dcaa544d684a54
CNLohr <lohr85@gmail.com>
2017-03-14 20:48:12 -0400

    Rename build script.

--- windows/build_tcc.bat
+++ windows/build_tcc.bat
@@ -1,10 +1,13 @@
 @echo off
+
+set TCC=C:\tcc\tcc.exe
+echo Be sure to have TinyCC installed to %TCC% (or change the TCC parameter.)
+
 set SR=..\src\
 set RD=..\redist\
 set SOURCES=%SR%ootx_decoder.c %SR%poser_charlesslow.c %SR%poser_daveortho.c %SR%poser_dummy.c %SR%survive.c %SR%survive_cal.c %SR%survive_config.c %SR%survive_data.c %SR%survive_driverman.c %SR%survive_process.c %SR%survive_usb.c
 set REDIST=%RD%crc32.c %RD%linmath.c %RD%puff.c %RD%jsmn.c %rd%json_helpers.c
 set EXEC=..\calibrate.c %RD%WinDriver.c %RD%os_generic.c %RD%DrawFunctions.c
-set TCC=C:\tcc\tcc.exe
 set CFLAGS=-DNOZLIB -DTCC -DWINDOWS -DHIDAPI -O0 -DWIN32 -g -rdynamic -I..\redist -I..\include\libsurvive -I..\src
 set LDFLAGS=-lkernel32 -lgdi32 -luser32
 @echo on
