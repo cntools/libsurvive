c9be7c057aa9570d0f67109c87b74e3aab640322
CNLohr <lohr85@gmail.com>
2017-03-17 00:42:05 -0400

    Update, adding rawdrawnull for OSX.

---
+++ redist/RawDrawNull.c
@@ -0,0 +1,74 @@
+//Copyright (c) 2017 <>< Charles Lohr - Under the MIT/x11 or NewBSD License you choose.
+
+#include "DrawFunctions.h"
+
+static int w, h;
+void CNFGGetDimensions( short * x, short * y )
+{
+	*x = w;
+	*y = h;
+}
+
+static void InternalLinkScreenAndGo( const char * WindowName )
+{
+}
+
+void CNFGSetupFullscreen( const char * WindowName, int screen_no )
+{
+	CNFGSetup( WindowName, 640, 480 );
+}
+
+
+void CNFGTearDown()
+{
+}
+
+void CNFGSetup( const char * WindowName, int sw, int sh )
+{
+	w = sw;
+	h = sh;
+}
+
+void CNFGHandleInput()
+{
+}
+
+
+void CNFGUpdateScreenWithBitmap( unsigned long * data, int w, int h )
+{
+}
+
+
+#ifndef RASTERIZER
+
+
+uint32_t CNFGColor( uint32_t RGB )
+{
+}
+
+void CNFGClearFrame()
+{
+}
+
+void CNFGSwapBuffers()
+{
+}
+
+void CNFGTackSegment( short x1, short y1, short x2, short y2 )
+{
+}
+
+void CNFGTackPixel( short x1, short y1 )
+{
+}
+
+void CNFGTackRectangle( short x1, short y1, short x2, short y2 )
+{
+}
+
+void CNFGTackPoly( RDPoint * points, int verts )
+{
+}
+
+#endif
+
