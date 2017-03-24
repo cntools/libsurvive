f7d1c19e299df46087eb8693d2d0c0637d2e395f
mwturvey <michael.w.turvey@intel.com>
2017-03-24 13:52:21 -0700

    Fix Calibrate visualization to use proper lighthouse data

--- calibrate.c
+++ calibrate.c
@@ -63,24 +63,24 @@ void my_light_process( struct SurviveObject * so, int sensor_id, int acode, int 
 	else if( strcmp( so->codename, "WM1" ) == 0 ) jumpoffset += 64;
 
 
-	if( acode == 0 || acode == 2 ) //data = 0
+	if( acode % 2 == 0 && lh == 0) //data = 0
 	{
 		bufferpts[jumpoffset*2+0][0] = (timeinsweep-100000)/500;
 		buffertimeto[jumpoffset][0] = 0;
 	}
-	if( acode == 1 || acode == 3 ) //data = 1
+	if(  acode % 2 == 1 && lh == 0 ) //data = 1
 	{
 		bufferpts[jumpoffset*2+1][0] = (timeinsweep-100000)/500;
 		buffertimeto[jumpoffset][0] = 0;
 	}
 
 
-	if( acode == 4 || acode == 6 ) //data = 0
+	if( acode % 2 == 0 && lh == 1 ) //data = 0
 	{
 		bufferpts[jumpoffset*2+0][1] = (timeinsweep-100000)/500;
 		buffertimeto[jumpoffset][1] = 0;
 	}
-	if( acode == 5 || acode == 7 ) //data = 1
+	if( acode % 2 == 1 && lh == 1 ) //data = 1
 	{
 		bufferpts[jumpoffset*2+1][1] = (timeinsweep-100000)/500;
 		buffertimeto[jumpoffset][1] = 0;
