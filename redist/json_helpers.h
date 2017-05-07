92d291674e5a12c7eedd314ffc4cf955d575cbea
Joshua Allen <axlecrusher@gmail.com>
2017-05-06 22:05:32 -0400

    add function to parse array of floats from json

--- redist/json_helpers.h
+++ redist/json_helpers.h
@@ -4,12 +4,16 @@
 #define JSON_HELPERS_H
 
 #include <stdint.h>
+#include <jsmn.h>
+#include "survive_types.h"
 
 void json_write_float_array(FILE* f, const char* tag, float* v, uint8_t count);
 void json_write_double_array(FILE* f, const char* tag, double* v, uint8_t count);
 void json_write_uint32(FILE* f, const char* tag, uint32_t v);
 void json_write_float(FILE* f, const char* tag, float v);
 void json_write_str(FILE* f, const char* tag, const char* v);
+
+int parse_float_array(char* str, jsmntok_t* token, FLT** values, uint8_t count);
 
 void json_load_file(const char* path);
 extern void (*json_begin_object)(char* tag);
