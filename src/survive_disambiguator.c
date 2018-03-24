ef7f9229a23e3ade8c6b5c33cbfaa684b80909b2
cnlohr <lohr85@gmail.com>
2018-03-24 03:44:02 -0400

    add option to log lightdata to separate file.

--- src/survive_disambiguator.c
+++ src/survive_disambiguator.c
@@ -1,3 +1,16 @@
 #include "survive.h"
+#include <os_generic.h>
+#include <stdio.h>
 
-void handle_lightcap(SurviveObject *so, LightcapElement *le) { so->ctx->lightcapfunction(so, le); }
+//#define LOG_LIGHTDATA
+
+void handle_lightcap(SurviveObject *so, LightcapElement *le)
+{
+#ifdef LOG_LIGHTDATA
+	static FILE * flog;
+	static double start = 0;
+	if( !flog ) { flog = fopen( "lightcap.txt", "wb" ); start = OGGetAbsoluteTime(); }
+	fprintf( flog, "%.6f %2d %4d %9d\n", OGGetAbsoluteTime()-start, le->sensor_id, le->length, le->timestamp );
+#endif
+	so->ctx->lightcapfunction(so, le);
+}
