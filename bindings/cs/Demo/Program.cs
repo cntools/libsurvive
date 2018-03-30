38730755d1cc6fb8dd8b0047a363a171d5ba148d
dpeter99 <dpeter99@gmail.com>
2018-03-30 20:54:08 +0200

    Some tests

--- bindings/cs/Demo/Program.cs
+++ bindings/cs/Demo/Program.cs
@@ -7,50 +7,59 @@ using System.Threading.Tasks;
 
 namespace Demo
 {
-    internal class MyHandler : SurviveContext
-    {
-        private static void WritePose(string name, SurvivePose pose)
-        {
-            Console.Out.WriteLine(name);
-            Console.Out.Write(" [ ");
-            for (int i = 0; i < 3; i++)
-                Console.Out.Write("{0} ", pose.Pos[i]);
-            Console.Out.Write(" ] [ ");
-            for (int i = 0; i < 4; i++)
-                Console.Out.Write("{0} ", pose.Rot[i]);
-            Console.Out.Write(" ] ");
-            Console.Out.WriteLine();
-        }
 
-        public MyHandler() : base()
-        {
-        }
+    //internal class MyHandler : SurviveContext
+    //{
+    //    private static void WritePose(string name, SurvivePose pose)
+    //    {
+    //        Console.Out.WriteLine(name);
+    //        Console.Out.Write(" [ ");
+    //        for (int i = 0; i < 3; i++)
+    //            Console.Out.Write("{0} ", pose.Pos[i]);
+    //        Console.Out.Write(" ] [ ");
+    //        for (int i = 0; i < 4; i++)
+    //            Console.Out.Write("{0} ", pose.Rot[i]);
+    //        Console.Out.Write(" ] ");
+    //        Console.Out.WriteLine();
+    //    }
 
-        public MyHandler(string[] args) : base(args)
-        {
-        }
+    //    public MyHandler() : base()
+    //    {
+    //    }
 
-        protected void LightHouseEvent1(IntPtr ctx, byte lighthouse, SurvivePose lighthouse_pose, SurvivePose object_pose)
-        {
-            base.LightHouseEvent(ctx, lighthouse, lighthouse_pose, object_pose);
-            WritePose("Lighthouse", lighthouse_pose);
-            WritePose("Object", object_pose);
-        }
+    //    public MyHandler(string[] args) : base(args)
+    //    {
+    //    }
+
+    //    protected void LightHouseEvent1(IntPtr ctx, byte lighthouse, SurvivePose lighthouse_pose, SurvivePose object_pose)
+    //    {
+    //        base.LightHouseEvent(ctx, lighthouse, lighthouse_pose, object_pose);
+    //        WritePose("Lighthouse", lighthouse_pose);
+    //        WritePose("Object", object_pose);
+    //    }
      
-        protected override void PoseEvent(IntPtr so, byte lighthouse, SurvivePose pose)
-        {
-            WritePose("Pose", pose);
-            base.PoseEvent(so, lighthouse, pose);
-        }
-    }
+    //    protected override void PoseEvent(IntPtr so, byte lighthouse, SurvivePose pose)
+    //    {
+    //        WritePose("Pose", pose);
+    //        base.PoseEvent(so, lighthouse, pose);
+    //    }
+    //}
+
     class Program
     {
         static void Main(string[] args)
         {
-            MyHandler handler = new MyHandler(args);
-            
-            while (handler.Poll() == 0) {
+            SurviveContext context = new SurviveContext("lightcap-reformat.log");
+            context.AddPoseUpdateCallback(HMDUpdate, -1);
+
+
+            while (context.Poll() == 0) {
             }
+
+        }
+
+        public static void HMDUpdate(int ObjectID, Vector3 pos)
+        {
 
         }
 
