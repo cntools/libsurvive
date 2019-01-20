f8901cfa6059d96b3f7012444e4f267a092ee6ba
Justin Berger <jdavidberger@gmail.com>
2019-01-20 06:32:54 +0000

    Made CLI signal handler linux specific

--- survive-cli.c
+++ survive-cli.c
@@ -1,7 +1,11 @@
 #include <survive.h>
-#include <signal.h>
 
 static volatile int keepRunning = 1;
+
+#ifdef __linux__
+
+#include <signal.h>
+#include <stdlib.h>
 
 void intHandler(int dummy) {
   if(keepRunning == 0)
@@ -9,11 +13,15 @@ void intHandler(int dummy) {
   keepRunning = 0;
 }
 
+#endif
+
 int main(int argc, char **argv) {
+#ifdef __linux__  
   signal(SIGINT, intHandler);
   signal(SIGTERM, intHandler);
   signal(SIGKILL, intHandler);  
-
+#endif
+  
   SurviveContext *ctx = survive_init(argc, argv);
 	if (ctx == 0) // implies -help or similiar
 		return 0;
