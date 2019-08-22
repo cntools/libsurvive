c3ec15d0990ca689aeec51769ec9f9f6c3c31fc8
Justin Berger <j.david.berger@gmail.com>
2019-08-21 22:47:00 -0600

    Adjusted test sensitivity

--- src/test_cases/test_replays.c
+++ src/test_cases/test_replays.c
@@ -18,7 +18,7 @@ static double complex diff(const SurvivePose *a, const SurvivePose *b) {
 
 static int test_path(const char *name, int main_argc, char **main_argv) {
 	int rtn = 0;
-	double max_pos_error = .0005, max_rot_error = .001;
+	double max_pos_error = .005, max_rot_error = .001;
 	char configPath[FILENAME_MAX] = {};
 	sprintf(configPath, "%s.json", name);
 
