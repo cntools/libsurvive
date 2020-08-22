33b9b674aa790dc8b34c814b58b1a7ad9648c7d1
Justin Berger <j.david.berger@gmail.com>
2020-08-22 16:36:52 -0600

    Fixed env variable for windows

--- useful_files/survive-websocketd.ps1
+++ useful_files/survive-websocketd.ps1
@@ -1,1 +1,1 @@
-websocketd --passenv HOME --port 8080 survive-cli --record-stdout --record-cal-imu --no-record-imu $args
+websocketd --passenv LOCALAPPDATA --port 8080 survive-cli --record-stdout --record-cal-imu --no-record-imu $args
