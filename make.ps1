e6989ce4704367683805ad3b7b704b7cf13242bd
Justin Berger <j.david.berger@gmail.com>
2020-08-22 09:34:00 -0600

    Added powershell scripts for windows

---
+++ make.ps1
@@ -0,0 +1,4 @@
+md build-win -ea 0
+cd build-win
+cmake ..
+cmake --build . --config Release
---
+++ useful_files/survive-websocketd.ps1
@@ -0,0 +1,1 @@
+websocketd --passenv HOME --port 8080 survive-cli --record-stdout --record-cal-imu --no-record-imu $args
