661ed65e8e0b385a21f5b3fa0988ed4305a7f9cb
Justin <j.david.berger@gmail.com>
2020-11-29 16:36:02 -0700

    Added long description

--- setup.py
+++ setup.py
@@ -5,17 +5,26 @@ dist.Distribution().fetch_build_eggs(['cmake_setuptools', 'scikit-build'])
 
 from skbuild import setup    
 import os
-
 import subprocess
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+      long_description = f.read()
+
+
 version = subprocess.check_output(["git", "describe", "--tags", "--long"]).strip().decode('utf-8').replace("-", ".")[1:]
 version = version[:version.rfind("g")-1]
 
 setup(name='pysurvive',
-      description='',
       version=version,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      description='Libsurvive is a set of tools and libraries that enable 6 dof tracking on lighthouse and vive based systems that is completely open source and can run on any device. It currently supports both SteamVR 1.0 and SteamVR 2.0 generation of devices and should support any tracked object commercially available.',
+      url='https://github.com/cntools/libsurvive',
       packages=['pysurvive'],
       package_dir={'pysurvive': 'bindings/python/pysurvive'},
       include_package_data=False,
+      license='MIT',
       cmake_args=['-DPYTHON_GENERATED_DIR="'+ os.path.dirname(os.path.abspath(__file__))+'/bindings/python/pysurvive/"',
 				  "-DBUILD_APPLICATIONS=OFF",
                   "-DLIB_INSTALL_DIR=bindings/python/pysurvive"]
