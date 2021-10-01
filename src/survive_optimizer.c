53cd0413ee8804d89a33efe60d5d3a08f45c7ea9
Ben Woodford <ben@fenneclabs.co.uk>
2021-10-01 10:04:26 +0100

    Fixing more MSVC Build Errors
    I don't necessarily _know_ that these are correct, but that's what PRs are for right? MSVC compiles with these fixes made on my end though...

--- src/survive_optimizer.c
+++ src/survive_optimizer.c
@@ -497,7 +497,7 @@ static int mpfunc(int m, int n, FLT *p, FLT *deviates, FLT **derivs, void *priva
 
 	for (size_t up_idx = 0; up_idx < meas_count - mpfunc_ctx->measurementsCnt; up_idx++) {
 		LinmathPoint3d up = {0};
-		LinmathAxisAngle rot = {};
+		LinmathAxisAngle rot = {0};
 		size_t m_idx = up_idx + mpfunc_ctx->measurementsCnt;
 
 		FLT bias = mpfunc_ctx->upVectorBias / (mpfunc_ctx->poseLength + mpfunc_ctx->cameraLength);
@@ -855,7 +855,7 @@ SURVIVE_EXPORT void survive_optimizer_setup_buffers(survive_optimizer *ctx, void
 	size_t measurementAllocSize =
 		ctx->poseLength * sizeof(survive_optimizer_measurement) * 2 * sensor_cnt * NUM_GEN2_LIGHTHOUSES;
 	memset(ctx->measurements, 0, measurementAllocSize);
-	ctx->obj_up_vectors = measurements_buffer + measurementAllocSize;
+	ctx->obj_up_vectors = (survive_optimizer_measurement *)measurements_buffer + measurementAllocSize;
 	ctx->cam_up_vectors = ctx->obj_up_vectors + ctx->poseLength;
 	memset(ctx->parameters_info, 0, sizeof(mp_par) * par_count);
 	for (int i = 0; i < survive_optimizer_get_parameters_count(ctx); i++) {
