cd97c2c3d91d84b76875bc8e911fd3ea66b32043
Justin Berger <j.david.berger@gmail.com>
2022-02-05 14:17:55 -0700

    Share safe_fns with cnkalman; fixs warnings

--- src/generated/common.h
+++ src/generated/common.h
@@ -1,19 +1,6 @@
 #pragma once
+#include "cnkalman/generated_header.h"
 #include "survive.h"
 #include <linmath.h>
 #include <math.h>
-static inline double __safe_sqrt(double x) { return x > 0 ? sqrt(x) : 0; }
-#undef sqrt
-#define sqrt __safe_sqrt
-static inline double __safe_asin(double x) { return asin(linmath_enforce_range(x, -1, 1)); }
-#undef asin
-#define asin __safe_asin
-#ifndef WIN32
-#ifndef ANDROID
-#include <complex.h>
-static inline double __safe_pow(double x, double y) { return x >= 0 ? pow(x, y) : creal(cpow(x, y)); }
-#undef pow
-#define pow __safe_pow
-#endif
-#endif
 #define GEN_FLT FLT
