cntools/libsurvive

commit 464dd9734a8de20df818fd83e7c1b016b068bb52

Fix windows wireless dongle setup

src/driver_vive.c
@@ -218,14 +218,17 @@ static uint8_t vive_magic_power_off[] = {
 static uint8_t vive_magic_raw_mode_1[] = {VIVE_REPORT_CHANGE_MODE, 0x01, 0x00, 0x00, 0x00};
 static uint8_t vive_request_version_info[] = {VIVE_REPORT_VERSION};
 
-static uint8_t vive_magic_rf_raw_mode_0[] = {
+// Windows needs 64 size for the wireless dongle
+static uint8_t vive_magic_rf_raw_mode_0[64] = {
 	VIVE_REPORT_COMMAND, VIVE_COMMAND_CHANGE_PROTOCOL, 0x6, 0x01, 0x00, 0x00, 0x02, 0x00, 0x00};
 static uint8_t vive_magic_rf_raw_mode_1[] = {
 	VIVE_REPORT_COMMAND, VIVE_COMMAND_CHANGE_PROTOCOL, 0x6, 0x01, 0x01, 0x00, 0x02, 0x00, 0x00};
-static uint8_t vive_magic_protocol_switch[] = {
+// Windows needs 64 size for the wireless dongle
+static uint8_t vive_magic_protocol_switch[64] = {
 	VIVE_REPORT_COMMAND, VIVE_COMMAND_CHANGE_PROTOCOL, 0x3, 0x00, 0x01, 0x00};
 static uint8_t vive_request_pairing[] = {VIVE_REPORT_COMMAND, VIVE_COMMAND_PAIR, 0x03, 0x01, 0x10, 0x27};
-static uint8_t vive_magic_protocol_super_magic[] = {VIVE_REPORT_COMMAND,
+// Windows needs 64 size for the wireless dongle
+static uint8_t vive_magic_protocol_super_magic[64] = {VIVE_REPORT_COMMAND,
 													VIVE_COMMAND_CONFIGURE_RADIO,
 													0x10,
 													0xbe,

src/driver_vive.hidapi.h
@@ -171,8 +171,10 @@ static survive_usb_device_t get_next_device(survive_usb_device_enumerator* d, su
 		return *d = head;
 	}
 	
+	// On Windows the wireless dongle interface is -1
+	// That's why we check for <= 0
 	for (survive_usb_device_t c = (*d)->next; c; c = c->next) {
-		if (c->interface_number == 0) {
+		if (c->interface_number <= 0) {
 			return *d = c; 
 		}
 	}

commit d2e608c6d64ddabf9c852377288391b08111b905

Fix sensor data loss on linux caused by 464dd97 (#294)

src/driver_vive.c
@@ -218,35 +218,50 @@ static uint8_t vive_magic_power_off[] = {
 static uint8_t vive_magic_raw_mode_1[] = {VIVE_REPORT_CHANGE_MODE, 0x01, 0x00, 0x00, 0x00};
 static uint8_t vive_request_version_info[] = {VIVE_REPORT_VERSION};
 
+#ifdef __linux__
+static uint8_t vive_magic_rf_raw_mode_0[] = {
+#else
 // Windows needs 64 size for the wireless dongle
 static uint8_t vive_magic_rf_raw_mode_0[64] = {
+#endif
 	VIVE_REPORT_COMMAND, VIVE_COMMAND_CHANGE_PROTOCOL, 0x6, 0x01, 0x00, 0x00, 0x02, 0x00, 0x00};
 static uint8_t vive_magic_rf_raw_mode_1[] = {
 	VIVE_REPORT_COMMAND, VIVE_COMMAND_CHANGE_PROTOCOL, 0x6, 0x01, 0x01, 0x00, 0x02, 0x00, 0x00};
+
+#ifdef __linux__
+static uint8_t vive_magic_protocol_switch[] = {
+#else
 // Windows needs 64 size for the wireless dongle
 static uint8_t vive_magic_protocol_switch[64] = {
+#endif
 	VIVE_REPORT_COMMAND, VIVE_COMMAND_CHANGE_PROTOCOL, 0x3, 0x00, 0x01, 0x00};
 static uint8_t vive_request_pairing[] = {VIVE_REPORT_COMMAND, VIVE_COMMAND_PAIR, 0x03, 0x01, 0x10, 0x27};
+
+#ifdef __linux__
+static uint8_t vive_magic_protocol_super_magic[] = {
+#else
 // Windows needs 64 size for the wireless dongle
-static uint8_t vive_magic_protocol_super_magic[64] = {VIVE_REPORT_COMMAND,
-													VIVE_COMMAND_CONFIGURE_RADIO,
-													0x10,
-													0xbe,
-													0x5b,
-													0x32,
-													0x54,
-													0x11,
-													0xcf,
-													0x83,
-													0x75,
-													0x53,
-													0x8a,
-													0x08,
-													0x6a,
-													0x53,
-													0x58,
-													0xd0,
-													0xb1};
+static uint8_t vive_magic_protocol_super_magic[64] = {
+#endif
+	VIVE_REPORT_COMMAND,
+	VIVE_COMMAND_CONFIGURE_RADIO,
+	0x10,
+	0xbe,
+	0x5b,
+	0x32,
+	0x54,
+	0x11,
+	0xcf,
+	0x83,
+	0x75,
+	0x53,
+	0x8a,
+	0x08,
+	0x6a,
+	0x53,
+	0x58,
+	0xd0,
+	0xb1};
 
 #define MAGIC_CTOR(ison, buffer)                                                                                       \
 	{ .name = #buffer, .code = ison, .magic = buffer, .length = sizeof(buffer) }