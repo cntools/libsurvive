9300403d677a7e964209aab1a67515c96f5c537b
Thomas Thron <tt@brandenburg-labs.com>
2024-06-20 11:22:24 +0200

    setup.py: refactor

--- setup.py
+++ setup.py
@@ -1,35 +1,39 @@
-import platform
-from setuptools import dist, find_packages
-# dist.Distribution().fetch_build_eggs(['wheel', 'cmake_setuptools'])
-
-# dist.Distribution().fetch_build_eggs(['cmake_setuptools', 'scikit-build'])
-
-from skbuild import setup    
 import os
+import platform
 import subprocess
+
+from setuptools import dist, find_packages
+from skbuild import setup
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-      long_description = f.read()
+    long_description = f.read()
 
 
-version = subprocess.check_output(["git", "describe", "--tags", "--long"]).strip().decode('utf-8').replace("-", ".")[1:]
+version = subprocess.check_output(["git", "describe", "--tags", "--long"])
+version = version.strip().decode('utf-8').replace("-", ".")[1:]
 version = version[:version.rfind("g")-1]
 
-cmake_args=['-DPYTHON_GENERATED_DIR="'+ os.path.dirname(os.path.abspath(__file__))+'/bindings/python/pysurvive/"',
-            "-DDOWNLOAD_EIGEN=ON",
-            "-DUSE_EIGEN=ON",
-	    "-DBUILD_APPLICATIONS=OFF",
-            "-DLIB_INSTALL_DIR=bindings/python/pysurvive/"]
+pysurvive_path = this_directory + '/bindings/python/pysurvive/'
+cmake_args = ['-DPYTHON_GENERATED_DIR="' + pysurvive_path + '"',
+              "-DDOWNLOAD_EIGEN=ON",
+              "-DUSE_EIGEN=ON",
+              "-DBUILD_APPLICATIONS=OFF",
+              "-DLIB_INSTALL_DIR=bindings/python/pysurvive/"]
 
 if platform.system() != 'Windows':
-      cmake_args.append('-DUSE_EIGEN=ON')
+    cmake_args.append('-DUSE_EIGEN=ON')
+
+description = """Libsurvive is a set of tools and libraries that enable 6 DoF tracking
+ on lighthouse and vive based systems that is completely open source and can run on
+ any device. It currently supports both SteamVR 1.0 and SteamVR 2.0 generation of
+ devices and should support any tracked object commercially available."""
 
 setup(name='pysurvive',
       version=version,
       long_description=long_description,
       long_description_content_type='text/markdown',
-      description='Libsurvive is a set of tools and libraries that enable 6 dof tracking on lighthouse and vive based systems that is completely open source and can run on any device. It currently supports both SteamVR 1.0 and SteamVR 2.0 generation of devices and should support any tracked object commercially available.',
+      description=description,
       url='https://github.com/cntools/libsurvive',
       packages=['pysurvive'],
       package_dir={'pysurvive': 'bindings/python/pysurvive'},
